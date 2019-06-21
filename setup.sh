611af6d58234d0d46b260a9e14b51614669e345c
SpecTurrican <lueths@gmail.com>
2019-06-21 22:04:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ OS=$(cat /etc/os-release | grep ID=raspian)
 
 if [ -n "$OS" ]; then
 
-	wget https://github.com/SpecTurrican/PI_BitCloud/tree/master/bitcloud_setup /root/bitcloud_setup/
+	git clone https://github.com/SpecTurrican/PI_BitCloud/tree/master/bitcloud_setup /root/bitcloud_setup/
 	nohup /root/bitcloud_setup/install_bitcloud.sh >/root/bitcloud_setup/logfile_start.log 2>&1 &
 	clear
 	tail -f /root/bitcloud_setup/logfile_start.log
