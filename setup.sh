e31242380e102c83988efd8021b579fa0eb04595
SpecTurrican <lueths@gmail.com>
2019-06-21 21:53:27 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OS=$(cat /etc/os-release | grep ID=raspian)
+
+if [ -n "$OS" ]; then
+
+	wget https://github.com/SpecTurrican/PI_BitCloud/tree/master/bitcloud_setup /root/bitcloud_setup/
+	nohup /root/bitcloud_setup/install_bitcloud.sh >/root/bitcloud_setup/logfile_start.log 2>&1 &
+	clear
+	tail -f /root/bitcloud_setup/logfile_start.log
+
+else
+
+	echo "This script running only below raspian ... sorry !!!"
+	echo " "
+	echo "Visit https://www.raspberrypi.org/downloads/raspbian/ "
+
+fi
